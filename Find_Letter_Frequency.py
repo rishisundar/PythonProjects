0f0d3b5c234f253fc7645f15c385fb79a340c25f
Rishisundar <rishi.pop.tfs@gmail.com>
2019-04-23 23:32:31 +0530

    Program to find letter frequency
    A simple python program that will find the frequency of all letters and the percentage of each from the file specified by the user.

---
+++ Find_Letter_Frequency.py
@@ -0,0 +1,19 @@
+import re
+fileName = raw_input("Enter the file name : ")
+input_file = open(fileName,"r")
+pattern = re.compile('[^a-zA-Z]+')
+dict = {}
+count = 0
+input = input_file.read()
+input = re.sub(pattern,'', input)
+for n in input:
+    keys = dict.keys()
+    if n in keys:
+        dict[n] += 1
+        count += 1
+    else:
+        dict[n] = 1
+        count += 1
+print ('Total Letters {}').format(count)
+for i in sorted(dict.keys()):
+    print("letter {} : Count : {:<4} Percentage : {:.2f}%".format(i,dict[i],float((dict[i]*100.0)/count)))
